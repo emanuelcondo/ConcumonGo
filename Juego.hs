a3b1a289bcf6825f65168a9d2ece7e64e7c3217d
martind <martindonofrio@gmail.com>
2016-11-09 10:15:28 -0300

    Agrego parte de la estructura de datos entre Juego y Jugador.

--- Juego.hs
+++ Juego.hs
@@ -3,6 +3,29 @@ module Juego where
 import Config
 import NidoConcumones
 import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad
+
+
+--Estructura que define el tablero
+data Posicion = Posicion
+    { x :: Int
+    , y :: Int
+    }
+    deriving (Show)
+
+data Estado = Libre
+            | Ocupado
+            | Concumon
+                deriving (Eq, Ord, Show)
+
+data Casillero = Casillero {
+        posicion :: Posicion,
+        estado :: Estado
+        }
+
+type Tablero = TVar [Casillero]
+
 
 main :: IO ()
 main = do
@@ -11,3 +34,5 @@ main = do
     tamMapa <- tamGrilla
     putStrLn ("[JGO]\tTamaño de la grilla: " ++ show tamMapa)
     putStrLn "[JGO]\tTerminando Juego"
+
+
--- Jugador.hs
+++ Jugador.hs
@@ -1,6 +1,30 @@
 module Jugador where
 
+-- Tipo que contabilizara la sumatoria de puntaje del jugador en la partida
+newtype Puntaje = Puntaje Int
+    deriving (Eq, Ord, Show)
+
+data Posicion = Posicion
+    { x :: Int
+    , y :: Int
+    }
+    deriving (Show)
+
+
 main :: IO ()
 main = do
     putStrLn "[JUG]\tSoy un nuevo jugador"
+
+    putStrLn "[JUG]\tMe muevo"
+
+    moverse
+
     putStrLn "[JUG]\tSaliendo del juego"
+
+
+-- Funcion que pone a mover el jugador en en tablero.
+moverse = do
+    putStrLn "[JUG]\t Estoy en posicion: (x,y) me muevo A: "
+    --Elijo aleatoreamente alguna direccion de casillero contiguo
+    --Hago un readTvar para ver si el casillero esta libre, sino repito.module
+
