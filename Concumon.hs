8e4dfd9fbf87100a407ab437889f7e5b0b518f80
emanuelcondo <emanuelcondo@gmail.com>
2016-11-29 12:14:29 -0300

    borrando cosas comentadas

--- Concumon.hs
+++ Concumon.hs
@@ -11,7 +11,7 @@ import Control.Monad.Cont
 import Data.Foldable (for_)
 import Data.Maybe
 
--- <<<<<<< HEAD
+
 main :: Posicion -> QSem -> TVar [[Int]] -> QSem -> Chan String -> TChan String -> IO ()
 main posicion semLeer sharedGrid semMaxConcu eventChannel logChan = do
     log' "Soy un nuevo concumón ... atrapame!!!" logChan
@@ -73,45 +73,6 @@ elegirProximaPosicion grid posiciones = do
                     return posAux
                 else
                     elegirProximaPosicion grid (tail posiciones)
-{-
-=======
-main :: QSem -> TVar [[Int]] -> QSem -> Chan String -> TChan String -> IO ()
-main semLeer sharedGrid semMaxConcu eventChannel logChan = do
-    log' "Soy un nuevo concumón... atrapame!!!" logChan
 
-    delay <- delayConcumons
-    log' ("Delay de " ++ show delay ++ " antes de moverme") logChan
-    threadDelay $ delay * 10^(6 :: Int)
-    moverse semLeer sharedGrid semMaxConcu logChan
-
-    -- Cuando es capturado, se "libera" un lugar para que pueda crearse otro concumón
-    log' "Fui atrapado :(" logChan
-    signalQSem semMaxConcu
-
-
-moverse :: QSem -> TVar [[Int]] -> QSem -> TChan String -> IO ()
-moverse semLeer sharedGrid semMaxConcu logChan = do
-    log' "Soy un concumon, me movi!" logChan
-{-    waitQSem semLeer
-    if (posActual == 0)
-        then do
-            return()
-    grid <- readTVar sharedGrid
-    pos <- buscarPosicionLibreAlrededor grid posActual
-    Grilla.updateGrid sharedGrid xi yi 0
-    Grilla.updateGrid sharedGrid xf yf 1
-    if (xi /= xf || yi /= yf)
-        log' $ "me movi a la Posición" xf xi
-    signalQSem semLeer
--}
-    moverse semLeer sharedGrid semMaxConcu logChan
-
--- TODO
--- buscarPosicionLibreAlrededor grid posActual = do
--- buscar hasta encontrar alguna posición libre
--- si no hay lugares libre volver con la misma Posicion
--- y no moverse
->>>>>>> 6f71fadfbe422da07ea87bfbd4e86d07d490690c
--}
 log' :: String -> TChan String -> IO ()
 log' = cgLog "CMN"
